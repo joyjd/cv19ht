157553e2a108e699a1bc10aa2ff2a72351ef027e
joyjd <joydas1611@gmail.com>
2020-08-11 23:10:29 +0530

    Updates

---
+++ precache-manifest.ee0c96a09f771340b353dfb83d9a8475.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f61ef31a3caba0bd43afb86739d637",
+    "url": "/cv19ht/index.html"
+  },
+  {
+    "revision": "440f56180fc8752f25d4",
+    "url": "/cv19ht/static/css/main.7ef521d8.chunk.css"
+  },
+  {
+    "revision": "c495115fe9f14a02c9e7",
+    "url": "/cv19ht/static/js/2.4a8662fa.chunk.js"
+  },
+  {
+    "revision": "c3f6561e55afe85a42ed1588281ea6cd",
+    "url": "/cv19ht/static/js/2.4a8662fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440f56180fc8752f25d4",
+    "url": "/cv19ht/static/js/main.89262af7.chunk.js"
+  },
+  {
+    "revision": "7098091923f2a77594f8",
+    "url": "/cv19ht/static/js/runtime-main.86e215a4.js"
+  },
+  {
+    "revision": "a688fca21bdcacce77a8aa612fec1e02",
+    "url": "/cv19ht/static/media/ambulance.a688fca2.png"
+  },
+  {
+    "revision": "e72a7e981778a6d672a27c7a6fd1a394",
+    "url": "/cv19ht/static/media/ambulance2.e72a7e98.png"
+  },
+  {
+    "revision": "150f9bdfadd5c2ba94eb2e513c4d15b0",
+    "url": "/cv19ht/static/media/hosp_header.150f9bdf.jpg"
+  },
+  {
+    "revision": "ec8aec7e840686e4a9caed865ed556f6",
+    "url": "/cv19ht/static/media/hospital_bg.ec8aec7e.jpg"
+  },
+  {
+    "revision": "d64636ff86dac68e4bd6979d63ddda06",
+    "url": "/cv19ht/static/media/loc_headBg.d64636ff.jpg"
+  },
+  {
+    "revision": "24b615381c441d2f48b6b9e8aa95ac88",
+    "url": "/cv19ht/static/media/mbl_bck_1.24b61538.jpg"
+  },
+  {
+    "revision": "a389f7d8f8f9723d7376f1496eff5d97",
+    "url": "/cv19ht/static/media/set_header.a389f7d8.jpg"
+  }
+]);
