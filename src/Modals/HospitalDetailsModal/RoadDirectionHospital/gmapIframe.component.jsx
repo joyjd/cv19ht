d5bfad696e912dd1d9265b0e62447f6656fd43fa
joyjd <joydas1611@gmail.com>
2020-08-13 21:14:52 +0530

    commit

--- src/Modals/HospitalDetailsModal/RoadDirectionHospital/gmapIframe.component.jsx
+++ src/Modals/HospitalDetailsModal/RoadDirectionHospital/gmapIframe.component.jsx
@@ -5,7 +5,7 @@ import GoogleCredentials from "./../../../Utils/GoogleCredentials";
 const iframe = "<iframe   width='100%' height='300px' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/directions?key=";
 
 const GmapIframe = (props) => {
-  return <div className='iframe-container' dangerouslySetInnerHTML={{ __html: iframe ? iframe + GoogleCredentials.apiKey + "&origin=" + props.userCords[0] + "," + props.userCords[1] + "&destination=" + props.hospitalCord[0] + "," + props.hospitalCord[1] + "&zoom=9&avoid=tolls|highways' allowfullscreen></iframe>" : "" }} />;
+  return <div className='iframe-container' dangerouslySetInnerHTML={{ __html: iframe ? iframe + GoogleCredentials.apiKey + "&origin=" + props.userCords[0] + "," + props.userCords[1] + "&destination=" + props.hospitalCord[0] + "," + props.hospitalCord[1] + "&zoom=6&avoid=tolls|highways' allowfullscreen></iframe>" : "" }} />;
 };
 
 const mapStateToProps = (state) => ({
