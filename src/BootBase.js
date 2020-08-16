f838011736758fbb879ce6b05eb6663f44621046
joyjd <joydas1611@gmail.com>
2020-08-17 02:18:41 +0530

    ss

--- src/BootBase.js
+++ src/BootBase.js
@@ -1,9 +1,6 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import WelcomeModal from "./Modals/WelcomeModal/WelcomeModal.component";
-//import App from "./App";
-
-const App = lazy(() => import("./App"));
-const renderLoader = () => <p>Loading</p>;
+import App from "./App";
 
 class Bootbase extends React.Component {
   constructor() {
@@ -31,7 +28,7 @@ class Bootbase extends React.Component {
   render() {
     return (
       <div>
-        <Suspense fallback={renderLoader()}>{this.state.enableAppModule ? <App showLocation={this.state.showLocation} /> : null}</Suspense>
+        {this.state.enableAppModule ? <App showLocation={this.state.showLocation} /> : null}
         <WelcomeModal open={this.state.openWelcomeAlert} onClose={(el) => this.handleCloseWelcomeAlert(el)} />
       </div>
     );
